2493963ce1796deb009dee74eaf4b25528398207
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-05-22 21:23:22 +0100

    create artifact

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 set -eu
 
 ICE_VERSION=${1:-"3.6.4"}
-tar cf - /opt/$ICE_VERSION/ | xz -z - > Ice-$ICE_VERSION-ubuntu1804-amd64.tar.xz
 
 pip download "zeroc-ice==$ICE_VERSION"
 tar -zxf "zeroc-ice-$ICE_VERSION.tar.gz"
 cd "zeroc-ice-$ICE_VERSION"
 python setup.py bdist_wheel
+
+tar cf - /opt/Ice-$ICE_VERSION/ | xz -z - > dist/Ice-$ICE_VERSION-ubuntu1804-amd64.tar.xz
+
 cp dist/* /dist/
