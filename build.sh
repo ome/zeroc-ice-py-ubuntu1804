596e9b5064f9d9a6f594e1e6e7a17ccd3388eb49
Simon Li <spli@dundee.ac.uk>
2019-11-19 18:02:41 +0000

    Build deps in Dockerfile, cpp and py build at runtime

--- build.sh
+++ build.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 ICE_VERSION=${1:-"3.6.5"}
 
+# Build ice cpp from source
+wget -q https://github.com/zeroc-ice/ice/archive/v$ICE_VERSION.tar.gz
+tar xzf v$ICE_VERSION.tar.gz
+cd ice-$ICE_VERSION/cpp
+
+make --silent && make install --silent
+
+tar cf - /opt/Ice-$ICE_VERSION/ | xz -z - > /dist/Ice-$ICE_VERSION-ubuntu1804-amd64.tar.xz
+
+# Zeroc IcePy
 pip3 download "zeroc-ice==$ICE_VERSION"
 tar -zxf "zeroc-ice-$ICE_VERSION.tar.gz"
 cd "zeroc-ice-$ICE_VERSION"
 python3 setup.py bdist_wheel
-
-tar cf - /opt/Ice-$ICE_VERSION/ | xz -z - > dist/Ice-$ICE_VERSION-ubuntu1804-amd64.tar.xz
 
 cp dist/* /dist/
